2396b6d1f99db9ee902996c664976f7f08d6f2a5
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-10-29 00:25:17 +0300

    the header file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
-
+import Header from "./components/Header.jsx";
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Home from "./pages/Home.jsx";
+// import About from "./pages/About.jsx";
+// import Contact from "./pages/Contact.jsx";
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import "./../Style/header.css";
+import { Link } from "react-router-dom";
+import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
+
+function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img src="/icon.png" alt="Logo" />
+      </div>
+
+      {/* Desktop Navigation */}
+      <div className="header__nav">
+        <Link to="/" className="header__link">
+          Home
+        </Link>
+        <Link to="/about" className="header__link">
+          About
+        </Link>
+        <Link to="/contact" className="header__link">
+          Contact Us
+        </Link>
+      </div>
+
+      {/* Mobile Menu */}
+      <div className="header__menuIcon">
+        <div
+          className="header__menuIcon__bar"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <FormatAlignJustifyIcon style={{ color: "#fff" }} />
+        </div>
+
+        <div
+          className={`header__menuIcon__dropdown ${menuOpen ? "active" : ""}`}
+        >
+          <Link
+            to="/"
+            className="header__link"
+            onClick={() => setMenuOpen(false)}
+          >
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className="header__link"
+            onClick={() => setMenuOpen(false)}
+          >
+            About
+          </Link>
+          <Link
+            to="/contact"
+            className="header__link"
+            onClick={() => setMenuOpen(false)}
+          >
+            Contact Us
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
