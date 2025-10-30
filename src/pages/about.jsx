573115c457a1fbb775e5336373839cc6e717f5b3
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-10-30 11:18:39 +0300

    css file of about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./../Style/about.css";
-import ancoote from "./../assets/images/ancoote.png";
+import ancoote from "./../assets/images/about.jpg";
 
 function About() {
   return (
@@ -9,15 +9,15 @@ function About() {
         className="about_image"
         style={{ backgroundImage: `url(${ancoote})`, maxWidth: "100%" }}
       >
-        About us
+        <h1>Oro Food</h1>
       </div>
       <div className="about_text">
-        <h2>About Food Choose</h2>
+        <h2>About Oro Food</h2>
         <p>
-          Food Choose is a platform dedicated to helping users find the best
-          food options based on their preferences. Whether you're looking for
-          healthy meals, quick snacks, or gourmet dishes, Food Choose has got
-          you covered.
+          Oro Food is a platform dedicated to helping users find the best food
+          options based on their preferences. Whether you're looking for healthy
+          meals, quick snacks, or gourmet dishes, Food Choose has got you
+          covered.
         </p>
         <p>
           Our mission is to make food selection easier and more enjoyable by
