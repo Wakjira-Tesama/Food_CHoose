db48d8969091900e6e8130368268b916352f5cbb
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-10-31 23:51:43 +0300

    contact file add

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import "./../Style/contact.css";
+import contact from "./../assets/images/contact.jpg";
 
 function Contact() {
   return (
-    <div className="contact">
-      <h1>Contact Us</h1>
-      <form>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Email:
-          <input type="email" name="email" />
-        </label>
-        <label>
-          Message:
-          <textarea name="message"></textarea>
-        </label>
-        <button type="submit">Send</button>
-      </form>
+    <div className="contact" style={{ backgroundImage: `url(${contact})` }}>
+      <div className="contact-pages">
+        <h1>Contact Us</h1>
+        <form>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <label>
+            Email:
+            <input type="email" name="email" />
+          </label>
+          <label>
+            Message:
+            <textarea name="message"></textarea>
+          </label>
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
   );
 }
